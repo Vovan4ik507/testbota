99685dd486b703501ca280b97eb42653b87b214d
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2019-11-24 17:15:13 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -1,17 +1,13 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands import Bot
 import os
 
-Bot = commands.Bot(command_prefix= '-')
-Bot.remove_command('help')
+Bot = commands.Bot(command_prefix = '-')
 
-@Bot.event
-async def on_ready():
-    print("Bot is online")
-
-@Bot.command(pass_context= True)
+@Bot.command()
 async def hello(ctx):
-    await Bot.send("Hello {}".format(ctx.message.author.mention))
+    await ctx.send("Hello {}".format(ctx.message.author.mention))
 
 token = os.environ.get('BOT_TOKEN')
 
