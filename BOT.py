0871c112247357e82632224765a3810299f318e1
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2019-11-24 18:06:52 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -10,6 +10,10 @@ clearly = False
 async def hello(ctx):
     await ctx.send("Hello {}".format(ctx.message.author.mention))
     await ctx.message.delete()
+    
+@Bor.command()
+async def clean(quant):
+    await channel.message.delete(int(quant))
 
 token = os.environ.get('BOT_TOKEN')
 
