e6f97778df40b6378f6155fe1eafd5b97075a34e
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2019-11-24 18:04:46 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -9,14 +9,7 @@ clearly = False
 @Bot.command()
 async def hello(ctx):
     await ctx.send("Hello {}".format(ctx.message.author.mention))
-    if clearly == True:
-        await ctx.message.delete()
-    
-@Bot.command()
-async def config(ctx, func):
-    if func == deliteble:
-        global clearly = True
-        
+    await ctx.message.delete()
 
 token = os.environ.get('BOT_TOKEN')
 
