42ea37fa1d475c2eec4c3cb53c9efd1488591647
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2019-11-26 18:48:51 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -9,21 +9,7 @@ clearly = False
 @Bot.command()
 async def hello(ctx):
     await ctx.send("Hello {}".format(ctx.message.author.mention))
-    await ctx.message.delete()
-    
-@Bot.command()
-async def info(ctx, user: discord.User):
-	emb = discord/Embed(title = "Info about {}".format(user.mention), color=0x39d0d6)
-	emd.add_field(name = "Name", value = user.mention)
-	emb.add_field(name = "Joined at", value = str(user.joined at)[:23])
-	emb.add_field(name = "ID", value = user.id)
-	if user.game != None:
-		emb.add_field(name = "Game", value = user.game)
-	emb.set_thumbnail(url = user.avatar_url)
-	emb.set_author(name = Bot.user.name, url = "https://discordapp.com/oauth2/authorize?client_id=513405344718782464&scope=bot&permissions=8")
-	emb.set_footer(text = "Вызвано: {}".format(user.mention), icon_url = user.avater_url)
-	await ctx.send(embed = emb)
-	await ctx.message.delete()    
+    await ctx.message.delete()    
     
 @Bot.command()
 async def clean(ctx):
