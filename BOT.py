97fa78f6f0721bf103873cbd5cdc726be5ed439b
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2019-11-29 18:51:44 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -14,8 +14,22 @@ async def hello(ctx):
 async def mute(ctx, member: discord.Member):
     mute_role = discord.utils.get(ctx.message.guild.roles, name="Mute")
     await member.add_roles(mute_role)
-    await ctx.send('Muted sucsesful')
+    await ctx.send('Muted successful')
     await ctx.message.delete()
+    
+@Bot.command()
+async def info(ctx, member: discord.Member):
+	emb = discord.Embed(title = "Info about {}".format(member.mention), color= 0x39d0d6)
+	emd.add_field(name = "Name", value = member.mention)
+	emb.add_field(name = "Joined at", value = str(member.joined at)[:23])
+	emb.add_field(name = "ID", value = member.id)
+	if user.game != None:
+		emb.add_field(name = "Game", value = member.game)
+	emb.set_thumbnail(url = member.avatar_url)
+	emb.set_author(name = discord.member.bot.name, url = "https://discordapp.com/oauth2/authorize?client_id=513405344718782464&scope=bot&permissions=8")
+	emb.set_footer(text = "Вызвано: {}".format(member.mention), icon_url = member.avatar_url)
+	await ctx.send(embed = emb)
+	await ctx.message.delete()
 
 token = os.environ.get('BOT_TOKEN')
 
