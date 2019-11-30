d1c4a3fc9e1db1937c8bb8871fcde39813e80b34
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2019-11-30 19:24:43 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -8,11 +8,14 @@ Bot = commands.Bot(command_prefix = '-')
 @Bot.command()
 async def hello(ctx):
     	await ctx.send("Hello {}".format(ctx.message.author.mention))
-    	await ctx.message.delete()    
+
+@Bot.command()
+async def say(ctx, arg):
+	await ctx.send(arg)
 
 @Bot.command()
 async def info(ctx, *, member: discord.Member):
-	emb = discord.Embed(title = "Info about {}".format(member.name), colour= 0x39d0d6)
+	emb = discord.Embed(title = "Info about {}".format(member.mention), colour= 0x39d0d6)
 	emd.add_field(name = "Name", value = member.name)
 	emb.add_field(name = "Joined at", value = str(member.joined_at)[:23])
 	emb.add_field(name = "ID", value = member.id)
@@ -22,7 +25,6 @@ async def info(ctx, *, member: discord.Member):
 	emb.set_author(name = "Vovan408#0901", url = "https://discordapp.com/oauth2/authorize?client_id=513405344718782464&scope=bot&permissions=8")
 	emb.set_footer(text = "Caused by: {}".format(member.mention), icon_url = member.avatar_url)
 	await ctx.send("1", embed = emb)
-	await ctx.message.delete()
         
 token = os.environ.get('BOT_TOKEN')
 
