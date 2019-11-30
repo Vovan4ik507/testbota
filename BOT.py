1f05de55e37a2906993057d23f27a9c9b21797b3
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2019-11-30 18:43:03 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -17,18 +17,7 @@ async def mute(ctx, member: discord.Member):
     	await ctx.send('Muted successful')
 	await ctx.message.delete()
     
-@Bot.command()
-async def info(ctx, member: discord.Member):
-	emb = discord.Embed(title = "Info about {}".format(member.mention), color= 0x39d0d6)
-	emd.add_field(name = "Name", value = member.mention)
-	emb.add_field(name = "Joined at", value = str(member.joined at)[:23])
-	emb.add_field(name = "ID", value = member.id)
-	if user.game != None:
-		emb.add_field(name = "Game", value = member.game)
-	emb.set_thumbnail(url = member.avatar_url)
-	emb.set_author(name = "Vovan408#0901", url = "https://discordapp.com/oauth2/authorize?client_id=513405344718782464&scope=bot&permissions=8")
-	emb.set_footer(text = "Caused by: {}".format(member.mention), icon_url = member.avatar_url)
-	await ctx.send(embed = emb)
+
 
 token = os.environ.get('BOT_TOKEN')
 
