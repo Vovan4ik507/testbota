3f6672d9c3a9eee7f0d5d1df1c76c53a5a353a53
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2019-12-01 12:00:18 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -16,13 +16,13 @@ async def say(ctx, arg):
 @Bot.command()
 async def info(ctx, member: discord.Member):
 	emb = discord.Embed(title = "Info about {}".format(member.mention), colour= "0x39d0d6")
-	emd.add_field(name = "Name", value = "{}".format(member.name))
-	emb.add_field(name = "Joined at", value = "{}".format(str(member.joined_at)[:23]))
-	emb.add_field(name = "ID", value = "{}".format(member.id))
-	emb.set_thumbnail(url = "{}".format(member.avatar_url))
+	emb.add_field(name = "Name", value = member.name)
+	emb.add_field(name = "Joined at", value = str(member.joined_at)[:23])
+	emb.add_field(name = "ID", value = member.id)
+	emb.set_thumbnail(url = member.avatar_url)
 	emb.set_author(name = "Vovan408#0901", url = "https://discordapp.com/oauth2/authorize?client_id=513405344718782464&scope=bot&permissions=8")
 	emb.set_footer(text = "Caused by: {}".format(member.mention), icon_url = member.avatar_url)
-	await ctx.send("1 {}".format(embed = emb))
+	await ctx.send(embed = emb)
         
 token = os.environ.get('BOT_TOKEN')
 
