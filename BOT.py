5cd99e2dafe919c0b241d4b1065ddc247959f3e7
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2019-12-01 11:42:41 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -14,18 +14,17 @@ async def say(ctx, arg):
 	await ctx.send(arg)
 
 @Bot.command()
-async def info(ctx, members: commands.Greedy[discord.Member]):
-	for member in members:
-		emb = discord.Embed(title = "Info about {}".format(member.mention), colour= 0x39d0d6)
-		emd.add_field(name = "Name", value = member.name)
-		emb.add_field(name = "Joined at", value = str(member.joined_at)[:23])
-		emb.add_field(name = "ID", value = member.id)
-		if member.game != None:
-			emb.add_field(name = "Game", value = member.game)
-		emb.set_thumbnail(url = member.avatar_url)
-		emb.set_author(name = "Vovan408#0901", url = "https://discordapp.com/oauth2/authorize?client_id=513405344718782464&scope=bot&permissions=8")
-		emb.set_footer(text = "Caused by: {}".format(member.mention), icon_url = member.avatar_url)
-		await ctx.send(embed = emb)
+async def info(ctx, member: discord.Member):
+	emb = discord.Embed(title = "Info about {}".format(member.mention), colour= "0x39d0d6")
+	emd.add_field(name = "Name", value = member.name)
+	emb.add_field(name = "Joined at", value = str(member.joined_at)[:23])
+	emb.add_field(name = "ID", value = member.id)
+	if member.game != None:
+		emb.add_field(name = "Game", value = member.game)
+	emb.set_thumbnail(url = member.avatar_url)
+	emb.set_author(name = "Vovan408#0901", url = "https://discordapp.com/oauth2/authorize?client_id=513405344718782464&scope=bot&permissions=8")
+	emb.set_footer(text = "Caused by: {}".format(member.mention), icon_url = member.avatar_url)
+	await ctx.send(embed = emb)
         
 token = os.environ.get('BOT_TOKEN')
 
