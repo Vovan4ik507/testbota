4c5348d5601d264083dbd984f6c8e37e17116205
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2019-12-01 12:22:07 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -15,7 +15,7 @@ async def say(ctx, *, arg):
 
 @Bot.command()
 async def info(ctx, member: discord.Member):
-	emb = discord.Embed(title = "Info about {}".format(member.mention), color = 0x39d0d6)
+	emb = discord.Embed(title = "Info about {}".format(member.mention)[1:], color = 0x39d0d6)
 	emb.add_field(name = "Name", value = member.name)
 	emb.add_field(name = "Joined at", value = str(member.joined_at)[:19])
 	emb.add_field(name = "ID", value = member.id)
