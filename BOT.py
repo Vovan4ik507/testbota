d33712d46f62154bf0ca60c1eda8f855213f643c
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2019-12-03 20:05:34 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -12,12 +12,15 @@ async def hello(ctx):
 @Bot.command()
 async def say(ctx, *, arg):
 	await ctx.send(arg)
+	
+@Bot.command()
+async def status(ctx, member: discord.Member):
+	await ctx.send(member.status)
 
 @Bot.command()
 async def info(ctx, member: discord.Member):
 	emb = discord.Embed(title = "Info about {}".format(member.mention), color = 0x39d0d6)
 	emb.add_field(name = "ID", value = member.id, inline = False)
-	emb.add_field(name = "Status", value = member.status, inline = False)
 	emb.add_field(name = "Joined server at", value = str(member.joined_at)[:19], inline = False)
 	emb.add_field(name = "Created account at", value = str(member.created_at)[:19], inline = False)
 	emb.add_field(name = "Highest role", value = member.roles, inline = False)
