1f867a6e2e1e668ae7c355ed42efa5c1b17e4ec7
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2019-12-03 20:12:13 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -19,13 +19,14 @@ async def status(ctx, member: discord.Member):
 
 @Bot.command()
 async def info(ctx, member: discord.Member):
-	emb = discord.Embed(title = "Info about {}".format(member.mention), color = 0x39d0d6)
+	emb = discord.Embed(title = member.mention, color = 0x39d0d6)
 	emb.add_field(name = "ID", value = member.id, inline = False)
 	emb.add_field(name = "Joined server at", value = str(member.joined_at)[:19], inline = False)
 	emb.add_field(name = "Created account at", value = str(member.created_at)[:19], inline = False)
 	emb.add_field(name = "Highest role", value = member.roles, inline = False)
+	emb.add_field(name = "Discriminator", value = member.discriminator, inline = False)
 	emb.set_thumbnail(url = member.avatar_url)
-	emb.set_author(name = "@Vovan#0901", url = "https://discordapp.com/oauth2/authorize?client_id=513405344718782464&scope=bot&permissions=8")
+	emb.set_author(name = member.name)
 	emb.set_footer(text = "Caused by: {}".format(member.name), icon_url = member.avatar_url)
 	await ctx.send(embed = emb)
         
