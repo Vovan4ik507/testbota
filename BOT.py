1800360e4acedd097acc31ca79903b9689608e60
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2019-12-03 20:20:53 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -14,12 +14,11 @@ async def say(ctx, *, arg):
 	await ctx.send(arg)
 	
 @Bot.command()
-async def info(ctx, member: discord.Member):
-	emb = discord.Embed(title = member.mention, color = 0x39d0d6)
+async def user(ctx, member: discord.Member):
+	emb = discord.Embed(title = '@' + member.name + '#' + str(member.discriminator), color = 0x39d0d6)
 	emb.add_field(name = "ID", value = member.id, inline = False)
 	emb.add_field(name = "Joined server at", value = str(member.joined_at)[:19], inline = False)
 	emb.add_field(name = "Created account at", value = str(member.created_at)[:19], inline = False)
-	emb.add_field(name = "Highest role", value = member.roles, inline = False)
 	emb.set_thumbnail(url = member.avatar_url)
 	emb.set_author(name = member.name + '#' + str(member.discriminator))
 	emb.set_footer(text = "Caused by: {}".format(member.name), icon_url = member.avatar_url)
