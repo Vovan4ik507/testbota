fd6e0bde43b20a12fbaeed46074b180e47128387
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2019-12-03 20:40:01 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import os
+import time
 
 Bot = commands.Bot(command_prefix = '-')
 
@@ -12,6 +13,21 @@ async def hello(ctx):
 @Bot.command()
 async def say(ctx, *, arg):
 	await ctx.send(arg)
+	
+@Bot.command()
+async def ping(ctx):
+    'Pings Bot'
+    channel = ctx.channel
+    t1 = time.perf_counter()
+    await channel.trigger_typing()
+    t2 = time.perf_counter()
+    latency = round(Bot.latency *1000)
+    t = round((t2-t1)*1000)
+    green = discord.Color.green()
+    desc=f":heartbeat: **{latency}**ms \n :stopwatch: **{t}**ms"
+    em = discord.Embed(title = ":ping_pong: Pong",description = desc, color = green)
+    em.set_footer(text=f"Requested by {ctx.author.name}",icon_url=ctx.author.avatar_url)
+    await ctx.send(embed=em)
 	
 @Bot.command()
 async def user(ctx, member: discord.Member):
