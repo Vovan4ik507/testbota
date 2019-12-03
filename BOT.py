6b50e9cabb2790a99144b5ffe1a1208ce426704c
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2019-12-03 21:23:59 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -35,7 +35,7 @@ async def user(ctx, member: discord.Member):
 	emb.add_field(name = "Joined server at", value = str(member.joined_at)[:19], inline = False)
 	emb.add_field(name = "Created account at", value = str(member.created_at)[:19], inline = False)
 	emb.set_thumbnail(url = member.avatar_url)
-	emb.set_author(name = f"Caused by: {member.name + '#' + str(member.discriminator)}", url = member.avatar_url)
+	emb.set_author(name = f"Caused by: {member.name + '#' + str(member.discriminator)}")
 	await ctx.send(embed = emb)
         
 token = os.environ.get('BOT_TOKEN')
