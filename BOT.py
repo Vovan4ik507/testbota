1a2603a92bd0840210bf683d99349b9b9b2efa8d
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2019-12-04 18:11:25 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -25,17 +25,24 @@ async def ping(ctx):
     green = discord.Color.green()
     desc=f":heartbeat: **{latency}**ms \n :stopwatch: **{t}**ms"
     em = discord.Embed(title = ":ping_pong: Pong",description = desc, color = green)
-    em.set_footer(text=f"Requested by {ctx.author.name}",icon_url=ctx.author.avatar_url)
-    await ctx.send(embed=em)
+    em.set_footer(text = f"Requested by {ctx.author.name}",icon_url=ctx.author.avatar_url)
+    await ctx.send(embed = em)
 	
 @Bot.command()
 async def user(ctx, member: discord.Member):
 	emb = discord.Embed(title = f"Info about {member.mention}", color = 0x39d0d6)
-	emb.add_field(name = "ID", value = member.id, inline = False)
-	emb.add_field(name = "Joined server at", value = str(member.joined_at)[:19], inline = False)
-	emb.add_field(name = "Created account at", value = str(member.created_at)[:19], inline = False)
-	emb.set_thumbnail(url = member.avatar_url)
-	emb.set_author(name = f"Caused by: {member.name + '#' + str(member.discriminator)}")
+	if member == None:
+		emb.add_field(name = "ID", value = ctx.message.author.id, inline = False)
+		emb.add_field(name = "Joined server at", value = str(ctx.message.author.joined_at)[:19], inline = False)
+		emb.add_field(name = "Created account at", value = str(ctx.message.author.created_at)[:19], inline = False)
+		emb.set_thumbnail(url = ctx.message.author.avatar_url)
+		emb.set_author(name = f"Caused by: {ctx.message.author.name + '#' + str(ctx.message.author.discriminator)}")
+	else:
+		emb.add_field(name = "ID", value = member.id, inline = False)
+		emb.add_field(name = "Joined server at", value = str(member.joined_at)[:19], inline = False)
+		emb.add_field(name = "Created account at", value = str(member.created_at)[:19], inline = False)
+		emb.set_thumbnail(url = member.avatar_url)
+		emb.set_author(name = f"Caused by: {member.name + '#' + str(member.discriminator)}")
 	await ctx.send(embed = emb)
         
 token = os.environ.get('BOT_TOKEN')
