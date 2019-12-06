63add84497b7f7ccceb0bcb6716bd9f0be6fd9b6
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2019-12-06 16:57:44 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -35,7 +35,10 @@ async def user(ctx, member: discord.Member):
 	emb.add_field(name = "ID", value = author.id, inline = False)
 	emb.add_field(name = "Joined server at", value = str(author.joined_at)[:19], inline = False)
 	emb.add_field(name = "Created account at", value = str(author.created_at)[:19], inline = False)
-	emb.add_field(name= "Highest role", value = author.top_role.mention, inline=False)
+	if author.top_role == everyone:
+		emb.add_field(name= "Highest role", value = author.top_role, inline=False)
+	else:
+		emb.add_field(name= "Highest role", value = author.top_role.mention, inline=False)
 	emb.set_thumbnail(url = author.avatar_url)
 	emb.set_author(name = f"Caused by: {author}", icon_url = author.avatar_url)
 	await ctx.send(embed = emb)
