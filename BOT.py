cb493f82415390c17adf63c5492d40720c565f63
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-01-29 17:47:02 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -40,11 +40,11 @@ async def ping(ctx):
 	
 @Bot.command()
 async def user(ctx, member: discord.Member):
-	emb = discord.Embed(title = str(member), color = 0x39d0d6)
+	emb = discord.Embed(title = member.mention, color = 0x39d0d6)
 	emb.add_field(name = "ID", value = member.id, inline = False)
 	emb.add_field(name = "Joined server at", value = str(member.joined_at)[:19], inline = False)
 	emb.add_field(name = "Created account at", value = str(member.created_at)[:19], inline = False)
-	emb.add_field(name = "Highest role", value = member.top_role, inline=False)
+	emb.add_field(name = "Highest role", value = member.top_role.mention, inline=False)
 	emb.set_thumbnail(url = member.avatar_url)
 	emb.set_author(name = f"Caused by: {str(ctx.author)}")
 	await ctx.send(embed = emb)
