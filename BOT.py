78f7583704049a6cf38ef61f10ec007f8d075fe3
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-02-06 08:50:21 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -38,8 +38,8 @@ async def roles(ctx, member: discord.Member):
 	pass
 
 @Bot.command()
-async def clear(ctx, amount):
-	await ctx.channel.purge(limit = amount)
+async def clear(ctx, amo):
+	await ctx.channel.purge(limit = amo)
 		
 @Bot.command()
 async def ping(ctx):
