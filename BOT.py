e005468012dd9c1ab64f9c2e277fae0142c403bf
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-02-06 17:35:51 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -62,7 +62,7 @@ async def user(ctx, member: discord.Member):
 		for i in range(47, len(info)):
     			if info[i] == 'd':
         			role_id = info[i + 2 : i + 20]
-				emb.add_field(name = '.', value = f'<@&{role_id}>', inline = True)
+				emb.add_field(name = 'o', value = f'<@&{role_id}>', inline = True)
 	else:
 		emb.add_field(name = 'Roles', value = member.roles[0], inline = False)
 	emb.set_thumbnail(url = member.avatar_url)
