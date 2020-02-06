c15d1a33cd7bcb07897f01924a0944afe76476ae
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-02-06 11:30:16 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -47,10 +47,14 @@ async def user(ctx, member: discord.Member):
     		f1.write(f'{member.roles}')
 	f2 = open(f'{member}.txt', 'r')
 	info = f2.read()
-	for i in range(0,len(info)):
-    		if info[i] == 'd':
-        		role_id = info[i + 2 : i + 20]
-        		emb.add_field(name = 'Roles', value = f'<@&{role_id}>', inline = False)
+	if len(info) > 48:
+		emb.add_field(name = 'Roles', value = member.roles[0], inline = True)
+		for i in range(48,len(info)):
+    			if info[i] == 'd':
+        			role_id = info[i + 2 : i + 20]
+        			emb.add_field(value = f'<@&{role_id}>', inline = True)
+	else:
+		emb.add_field(name = 'Roles', value = member.roles[0], iline = False
 	emb.set_thumbnail(url = member.avatar_url)
 	emb.set_footer(text = f"Caused by: {str(ctx.author)}", icon_url = ctx.author.avatar_url)
 	await ctx.send(embed = emb)
