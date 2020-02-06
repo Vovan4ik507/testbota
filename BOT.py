39c73108b82e0330dcd8e0ca7c8d143a9cf5a515
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-02-06 10:21:16 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -66,7 +66,7 @@ async def ping(ctx):
 	
 @Bot.command()
 async def user(ctx, member: discord.Member):
-	emb = discord.Embed(title = str(member), description = member.mention, color = member.top_role.color)
+	emb = discord.Embed(title = member.top_role.mention, description = member.mention, color = member.top_role.color)
 	emb.add_field(name = "ID", value = member.id, inline = False)
 	emb.add_field(name = "Joined server at", value = str(member.joined_at)[:19], inline = False)
 	emb.add_field(name = "Created account at", value = str(member.created_at)[:19], inline = False)
