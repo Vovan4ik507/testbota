19bee5a9ab129ba33659fde74fccd28f176bc028
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-08-16 10:43:52 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -38,7 +38,6 @@ async def user(ctx, member: discord.Member):
 		emb.add_field(name = "Highest role", value = member.top_role, inline = False)
 	else:
 		emb.add_field(name = "Highest role", value = member.top_role.mention, inline = False)
-        emb.add_field(name = 'Roles', value = member.roles[0:len(member.roles)], inline = False)
 	emb.set_thumbnail(url = member.avatar_url)
 	emb.set_footer(text = f"Caused by: {str(ctx.author)}", icon_url = ctx.author.avatar_url)
 	await ctx.send(embed = emb)
