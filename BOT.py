2ac98763186cb48b8bcba03da31b1aceec2641a5
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-08-22 12:04:10 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -13,17 +13,17 @@ async def user(ctx, member: discord.Member):
 		date = str(member.joined_at)[8:10] + 'st'
 	else:
 		date = str(member.joined_at)[8:10]
-    	emb = discord.Embed(title = str(member), description = member.mention, color = member.top_role.color)
-    	emb.add_field(name = "ID", value = member.id, inline = False)
-    	emb.add_field(name = "Joined server at", value = str(member.joined_at)[8:10], inline = False)
-    	emb.add_field(name = "Created account at", value = str(member.created_at)[:19], inline = False)
-    	if member.top_role == member.roles[0]:
-        	emb.add_field(name = "Highest role", value = member.top_role, inline = False)
-    	else:
-        	emb.add_field(name = "Highest role", value = member.top_role.mention, inline = False)
-    	emb.set_thumbnail(url = member.avatar_url)
-    	emb.set_footer(text = f"Caused by: {str(ctx.author)}", icon_url = ctx.author.avatar_url)
-    	await ctx.send(embed = emb)
+	emb = discord.Embed(title = str(member), description = member.mention, color = member.top_role.color)
+	emb.add_field(name = "ID", value = member.id, inline = False)
+	emb.add_field(name = "Joined server at", value = date, inline = False)
+	emb.add_field(name = "Created account at", value = str(member.created_at)[:19], inline = False)
+	if member.top_role == member.roles[0]:
+		emb.add_field(name = "Highest role", value = member.top_role, inline = False)
+	else:
+		emb.add_field(name = "Highest role", value = member.top_role.mention, inline = False)
+	emb.set_thumbnail(url = member.avatar_url)
+	emb.set_footer(text = f"Caused by: {str(ctx.author)}", icon_url = ctx.author.avatar_url)
+	await ctx.send(embed = emb)
 
 @Bot.event
 async def on_ready():
