2cc0255f990f938bc8572d6d4012345a2488c461
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-08-22 10:48:53 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -10,7 +10,12 @@ Bot = commands.Bot(command_prefix = '-')
 async def user(ctx, member: discord.Member):
     	emb = discord.Embed(title = str(member), description = member.mention, color = member.top_role.color)
     	emb.add_field(name = "ID", value = member.id, inline = False)
-    	emb.add_field(name = "Joined server at", value = str(member.joined_at)[:19], inline = False)
+#	value = str(member.joined_at)[:19]
+	if str(member.joined_at)[8:10] == '01' or str(member.joined_at)[8:10] == '21' or str(member.joined_at)[8:10] == '31':
+		day_index = 'st'
+	else:
+		day_index = ''
+    	emb.add_field(name = "Joined server at", value = str(member.joined_at)[8:10] + day_index, inline = False)
     	emb.add_field(name = "Created account at", value = str(member.created_at)[:19], inline = False)
     	if member.top_role == member.roles[0]:
         	emb.add_field(name = "Highest role", value = member.top_role, inline = False)
