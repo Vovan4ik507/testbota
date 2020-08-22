886e27a6e2e3a5e9903092c6e512fbcd4bf4f1db
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-08-22 10:16:09 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -5,7 +5,21 @@ import os
 import time
 
 Bot = commands.Bot(command_prefix = '-')
-        
+
+@Bot.command()
+async def user(ctx, member: discord.Member):
+    	emb = discord.Embed(title = str(member), description = member.mention, color = member.top_role.color)
+    	emb.add_field(name = "ID", value = member.id, inline = False)
+    	emb.add_field(name = "Joined server at", value = str(member.joined_at)[:19], inline = False)
+    	emb.add_field(name = "Created account at", value = str(member.created_at)[:19], inline = False)
+    	if member.top_role == member.roles[0]:
+        	emb.add_field(name = "Highest role", value = member.top_role, inline = False)
+    	else:
+        	emb.add_field(name = "Highest role", value = member.top_role.mention, inline = False)
+    	emb.set_thumbnail(url = member.avatar_url)
+    	emb.set_footer(text = f"Caused by: {str(ctx.author)}", icon_url = ctx.author.avatar_url)
+    	await ctx.send(embed = emb)
+
 @Bot.event
 async def on_ready():
 	print('Bot is online')
