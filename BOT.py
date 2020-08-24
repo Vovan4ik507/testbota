311b8bf2ad72810e93ce534011441aa14d72d49f
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-08-24 16:48:49 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -44,11 +44,11 @@ async def user(ctx, member = None):
 	
 	for i in range(0, len(member_list)):
 		mention1= member_list[i].mention
-		if mention1[i] == '@' and mention1[i + 1] in number:
-			mention2 = mention1[0:i + 1] + '!' + mention1[i + 1: len(mention1)]
-			mention_list.append(mention2)
-		else:
+		if mention1[i] == '@' and mention1[i + 1] == '!':
 			mention_list.append(mention1)
+		else:
+			mention2 = mention1[0:i + 1] + '!' + mention1[i + 1: len(mention1)]
+			mention_list.append(mention2)	
 	
 	for i in range(0, len(member_list)):
 		if member == member_list[i].name or member == str(member_list[i].id) or member == mention_list[i]:
