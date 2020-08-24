afd424b52da5e65125b6d98ec14f4df2248059d0
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-08-24 16:06:08 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -39,11 +39,11 @@ async def user(ctx, member = None):
 	member_stop = False
 	
 	for i in range(0, len(member_list)):
-		if member == member_list[i].name or member == member_list[i].id or member == str(member_list[i].mention):
+		if member == member_list[i].name or member == str(member_list[i].id) or member == str(member_list[i].mention):
 			member_stop = True
 			member = member_list[i]
 		else:
-			await ctx.send(f'{member_list[i].name}, {member_list[i].id}, {member_list[i].mention}')
+			await ctx.send(f'{member_list[i].name}, {member_list[i].id}, `{member_list[i].mention}`')
 	else:
 		if member_stop == False:
 			if member == None:
