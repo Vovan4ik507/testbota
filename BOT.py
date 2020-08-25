8a3bbe89da4190c818b2c6b2a370e8ba0c82915e
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-08-25 20:24:28 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -88,6 +88,44 @@ async def clean(ctx, channel = None, msgs = None):
 					await ctx.channel.purge(limit = 1)
 					await ctx.send(f'Deleted {len(deleted)} messages')
 
+@Bot.command()
+async def avatar(ctx, member = None):
+	if member == None:
+		member = ctx.author
+	else:
+		guild = ctx.guild
+		member_list = guild.members
+		member_stop = False
+		mention1 = ''
+		mention2 = ''
+		mention_list = []
+
+		author_mention = str(member)
+		if author_mention[1] == '@' and author_mention[2] != '!':
+			author_mention = member[0:2] + '!' + member[2 : len(member)]
+			member = author_mention
+
+		for i in range(0, len(member_list)):
+			mention1 = member_list[i].mention
+			if mention1[1] == '@' and mention1[2] == '!':
+				mention_list.append(mention1)
+			else:
+				mention2 = mention1[0 : 2] + '!' + mention1[2 : len(mention1)]
+				mention_list.append(mention2)	
+
+		for i in range(0, len(member_list)):
+			if member == member_list[i].name or member == str(member_list[i].id) or member == mention_list[i]:
+				member_stop = True
+				member = member_list[i]
+		else:
+			if member_stop == False:
+				if member != None:
+					await ctx.send('You wrote member index incorectly.')
+	a_e = discord.Embed(title = f'{member} avatar', color = discord.Color.green())
+	a_e.set_image(url = member.avatar_url)
+	a_e.set_footer(text = f'Caused by: {ctx.author}', icon_url = ctx.author.avatar_url)
+	await ctx.send(embed = a_e)
+
 def calculator(num):
 	
 	num = str(num)
